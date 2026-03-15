85d6804e99a3e5dc3f22f3cb3b9931fccaf8c1f9
Nikola Severov <severovnikola8@gmail.com>
2026-03-15 21:37:35 +0100

    Added character art to end screen

--- Assets/Scripts/EndScreenManager.cs
+++ Assets/Scripts/EndScreenManager.cs
@@ -7,12 +7,30 @@ public class EndScreenManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI winnerText;
 
+    [SerializeField] Image winnerImage;
+    
+    [SerializeField] Sprite brawlerArt;
+    [SerializeField] Sprite swordsmanArt;
+    [SerializeField] Sprite gunslingerArt;
+
     void Start()
     {
         if (GameData.winner == 1) {
             winnerText.text = "PLAYER 1 WINS!";
+            AssignImage(GameData.player1Character);
         } else if (GameData.winner == 2) {
             winnerText.text = "PLAYER 2 WINS!";
+            AssignImage(GameData.player2Character);
+        }
+    }
+
+    private void AssignImage(string character) {
+        if (character == "Brawler") {
+            winnerImage.sprite = brawlerArt;
+        } else if (character == "Swordsman") {
+            winnerImage.sprite = swordsmanArt;
+        } else if (character == "Gunsinger") {
+            winnerImage.sprite = gunslingerArt;
         }
     }
 
